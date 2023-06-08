4ad1d539a78417434f8190b319ffeee17ea0849f
Jenny Zambrano <jennyzambrano81@gmail.com>
2023-06-08 19:42:08 -0300

    cambiando nuevamente URL

--- pages/tycs.tsx
+++ pages/tycs.tsx
@@ -53,7 +53,7 @@ const {MAIN} =
 
 export const getStaticProps : GetStaticProps = async ({locale}) => {
 
-  const baseURL = "https://ctd-fe3-s3-c9-integracion-tienda-libre-base"
+  const baseURL = "http://localhost:3000/"
  
   const response = await fetch(`${baseURL}/api/tycs/${locale}`);
   const data = await response.json();
